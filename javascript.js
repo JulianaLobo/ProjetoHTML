235c42fcd379218b69bf1c2bf7d970fa80ed29ac
Juliana Lobo <sousalobojuliana@gmail.com>
2021-03-29 20:58:06 -0300

    Tipos de Dados

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 alert("Alertaaaaaa");
+
+var nome = "Juliana"; //string
+var idade = 20; //number
+var salvo = true; //boolean
+var cidade; //indefined
+
+/*
+Mostra no console a atribuição da variável
+console.log(nome); 
+
+Mostra no console o tipo de variável
+typeof nomeVariavel;
+*/
+
+var tipo = typeof idade; //Atribuindo à variável tipo o nome do tipo da variável idade
+console.log(tipo);
